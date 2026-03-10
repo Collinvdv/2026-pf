43365cda9b5b8ef2255c5c1f7bd8fc8e5d6e2653
Collin Van der Vorst <collin@littlemissrobot.com>
2026-03-10 12:41:12 +0100

    ADDED WEEK 4

---
+++ week4/recap/Program.cs
@@ -0,0 +1,126 @@
+﻿namespace recap;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Color 
+        // I: yellow 
+        // O: banana
+
+        // Color 
+        // I: green 
+        // O: apple
+
+        // string color = Console.ReadLine().ToLower();
+
+        // if (color == "yellow")
+        // {
+        //     Console.WriteLine("Banana");
+        // } else if (color == "green")
+        // {
+        //     Console.WriteLine("Apple");
+        // } else
+        // {
+        //     Console.WriteLine("I don't know this color");
+        // }
+
+        // switch(color)
+        // {
+        //     case "yellow":
+        //         Console.WriteLine("Banana");
+        //         break;
+        //     case "green":
+        //         Console.WriteLine("pple");
+        //         break;
+        //     default: 
+        //         Console.WriteLine("I don't know the color");
+        //         break;
+        // }
+
+
+
+
+        // I: 14
+        // O: No enter
+
+        // I: 18 
+        // O: Enter
+        // // Convert.ToDouble("18") -> (double)18
+        // double age = Convert.ToDouble(Console.ReadLine());
+        // char gender = Convert.ToChar(Console.Read()); //'f'
+        // Console.WriteLine(Char.IsLower(gender));
+        // if (age >= 18 && age <= 30 && (gender == 'f' || gender == 'F'))
+        // {
+        //     Console.WriteLine("Enter"); 
+        // } else
+        // {
+        //     Console.WriteLine("Don't enter");
+        // }
+
+        string ageStr = Console.ReadLine();
+
+        int age;
+
+        bool canIParse = int.TryParse(ageStr, out age);
+
+        if (canIParse)
+        {
+            Console.WriteLine("Parseable to int");
+        } else
+        {
+            Console.WriteLine("Not parseable to int");
+        }
+
+
+        // Write a program that checks a username and password.
+        // If the username is "admin" and the password is "1234", print Access granted. Otherwise print Access denied.
+
+        // O: Username:
+        // I: admin
+        // O: Password: 
+        // I: 1234
+        // O: Access granted
+        Console.WriteLine("Username");
+        string username = Console.ReadLine();
+        Console.WriteLine("Password");
+        string pass = Console.ReadLine();
+
+        if (username == "admin" && pass == "1234")
+        {
+            Console.WriteLine("Access granted");
+        } else
+        {
+            Console.WriteLine("Access denied");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// Exercise 1 – Discount Check
+
+// Write a program that checks if a customer gets a discount.
+// If the purchase amount is 100 or more, the customer gets a discount message. Otherwise, show that there is no discount.
+
+// I: 120
+// O: Discount applied
+
+// Exercise 2 – Login Check
+
+// Write a program that checks a username and password.
+// If the username is "admin" and the password is "1234", print Access granted. Otherwise print Access denied.
+
+// I:
+// Username: admin
+// Password: 1234
+
+// O: Access granted
---
+++ week4/theory/Program.cs
@@ -0,0 +1,173 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Dynamic;
+using System.Text;
+
+namespace theory;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // For loop
+        // When? you now the start and end
+        // how? start;expression;step
+        // 10... 9... 8... 7.. 1
+
+        // All the even numbers between 1 and 100 ; 
+        // O: Start number
+        // I: 4
+        // O: End number
+        // I: 65
+        // int start = Convert.ToInt16(Console.ReadLine());
+        // int end = Convert.ToInt16(Console.ReadLine());
+
+        // // Check if start is smaller than end
+        // if (start < end)
+        // {
+        //     // I willl count from start to end and do +1
+        //     for (int i = start; i <= end; i++)
+        //     {
+        //         // I will check if the remainder is 0, 
+        //         // then it is even
+        //         if (i % 2 == 0)
+        //         {
+        //             Console.WriteLine($"{i}");
+        //         }
+        //     }
+        // }
+
+        // O: 0.1
+        // O: 0.2
+        // O: 0.3
+        // O: 0.4
+        // O: 0.5
+        // O: 0.6
+        // O: 0.7
+        // O: 0.8
+        // O: 0.9
+        // O: 1
+
+        // decimal result = 0m;
+        // for (int i = 1; i <= 10; i++)
+        // {
+        //     result +=0.1m;
+        //     Console.WriteLine($"{i} {result}");
+        // }
+
+        // Exercise 3
+        // I: An integer n
+        // O: Print the multiplication table of n from 1 to 10.
+
+        // Example output for n = 3:
+
+        // 3 x 1 = 3
+        // 3 x 2 = 6
+        // ...
+        // 3 x 10 = 30
+        // int userNumber = Convert.ToInt16(Console.ReadLine());
+
+        // for(int i = 1; i <= 10; i++)
+        // {
+        //     int result = i * userNumber;
+        //     Console.WriteLine($"{userNumber} x {i} = {result}");
+        // }
+
+        // Exercise 6
+
+        // I: An integer n
+        // O: Print a triangle of stars with height n.
+
+        // Example for n = 4:
+
+        // ****
+        // ***
+        // **
+        // *
+
+        // 1: *
+        // 2: **
+        // 3: ***
+        // 4: ****
+
+        // int n = 4;
+
+        // // amount of rows
+        // for (int row = 1; row <= n; row++)
+        // {
+        //     // calculate the amount of stars 
+        //     // I see the stars are the same as the row number
+        //     for (int col = 1; col <= row; col++)
+        //     {
+        //         Console.Write("*");
+        //     }
+
+        //     Console.WriteLine();
+        // }
+
+        // int number = 1;
+
+        // while (number <= 10)
+        // {
+        //     Console.WriteLine(number);
+
+        //     number++;
+        // }
+
+        // secret word is knockknock
+        // O: Give me the secret word 
+        // I: apple
+        // O: no, give me the secret word
+        // I: banana
+        // O: no, give me the secret word
+        // I: knockknock
+        // O: Enter
+
+        // string secretPassword = Console.ReadLine();
+
+        // while (secretPassword != "knockknock")
+        // {
+        //     Console.WriteLine("no, give me the secret word");
+
+        //     secretPassword = Console.ReadLine();
+        // }
+
+        // Console.WriteLine("Enter");
+
+        // I: 5
+        // O: total(5) is not bigger than 100 continue
+        // I: 90
+        // O: total(95) is not bigger than 100 continue
+        // I: 10
+        // O: Total is now more than 100
+
+        // double number = Convert.ToDouble(Console.ReadLine());
+        // double total = number;
+
+        // do
+        // {
+        //     Console.WriteLine($"total({total}) is not bigger than 100 continue");
+        //     number = Convert.ToDouble(Console.ReadLine());
+
+        //     total += number;
+        // } while (total < 100);
+ 
+
+        // Console.WriteLine("Total is now more than 100");
+        int number = Convert.ToInt16(Console.ReadLine()); 
+        string numberStr = number.ToString();
+
+        if (number >= 0)
+        {            
+            for (int i = (numberStr.Length-1); i >= 0; i--)
+            {
+            Console.Write(numberStr[i]);
+            }
+        } else
+        {
+            Console.WriteLine("Negative numver");
+        }
+
+
+    }
+}
