bb156366c3d706307666cda0b2c5f4f71582918d
Collin Van der Vorst <collin@littlemissrobot.com>
2026-05-12 09:23:50 +0200

    Added the recap

---
+++ week11_methods/inclass_assinments/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace inclass_assinments;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+// 04 BigOrSmallPond
+// 04 BigOrSmallPond
+
+// Read in a value that will determine the depth of a pond. If the pond is 4 deep, the bottom will be at that 4th level.
+// Tip: try to look for patterns and don't be afraid to split this up into sections.
+
+ 
+
+// Input/Output
+// input:
+// 5
+// output:
+// 0 ~ ~ ~ ~ ~ ~ ~ ~ 0
+// 0 0 ~ ~ ~ ~ ~ ~ 0 0
+// 0 0 0 ~ ~ ~ ~ 0 0 0
+// 0 0 0 0 ~ ~ 0 0 0 0
+// 0 0 0 0 0 0 0 0 0 0
+
+// input:
+// 3
+// output:
+// 0 ~ ~ ~ ~ 0
+// 0 0 ~ ~ 0 0
+// 0 0 0 0 0 0
+
+// 04 Mix it up!
+// 04 Mix it up!
+
+// Read in two strings and create a new word by mixing both strings.
+// Start with the longest word and alternately take a character of both words until all characters are used in the mix.
+// (tip: work with the length of a word and ElementAt() or indexes)
+// Next to using System; at the top of your code, you might also have to include using System.Linq;
+
+ 
+
+// Input/Output
+// input:
+// Taylor
+// Swift
+// output:
+// TSawyilfotr
+
+// input:
+// <312>
+// DancinG
+// output:
+// D<a3n1c2i>nG
+
+
+// 05 Reverse
+// 05 Reverse
+
+// Read a string and transform it to an array. Take each word from that array and reverse the words. Print out the new array.
+
+// Note: use of Linq-library or Array-library is forbidden!
+
+ 
+
+// Input/Output
+// input:
+// Once I make my move, the Queen will take me. Then you're free to check the king.
+// output:
+// ecnO I ekam ym ,evom eht neeuQ lliw ekat .em nehT er'uoy eerf ot kcehc eht .gnik
+
+// input:
+// No. Ron, NO! What is it? He's going to sacrifice himself. No, you can't, there must be another way!
+// output:
+// .oN ,noR !ON tahW si ?ti s'eH gniog ot ecifircas .flesmih ,oN uoy ,t'nac ereht tsum eb rehtona !yaw
+
+
+// 05 Swap
+// 05 Swap
+
+// Read in an intiger, indicating a square array. Next, read in every element of this 2D-array string by string, split with a space.
+// Lastly, swap around the rows and columns and print out your new array.
+
+// Note: use of Linq-library or Array-library is forbidden!
+
---
+++ week11_methods/recap/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace recap;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string lyrics = "I got, got, got I-I-I You got a death wish Got a death wish Someone said Someone said, 'Hey, you got a death wish";
+
+        string[] words = lyrics.Split(' ');
+
+        // Loop over every word 
+        foreach(string word in words)
+        {
+            // Lets create new words without capitals and in lower case
+            string cleanedWord = RemoveSymbols(word);
+            Console.WriteLine(cleanedWord);
+        }
+    }
+
+    public string RemoveSymbols(string _word)
+    {
+        string cleanedWord = "";
+        foreach(char c in _word)
+        {
+            if (Char.IsLetter(c))
+            {
+                cleanedWord += c;
+            }
+        }
+
+        return cleanedWord;
+    }
+}
